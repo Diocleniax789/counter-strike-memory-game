432f20b3680f67cd2d745d27198a1165960781f4
Diocleniax789 <julioramdom123@gmail.com>
2024-10-27 02:24:28 -0300

    se ha creado el menu principal del juego

--- counter-strike-memory-game/cs-memory-game.c
+++ counter-strike-memory-game/cs-memory-game.c
@@ -1,10 +1,38 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main(){
+    int op;
 
-
-
+    do{
+        system("cls");
+        printf("\n -------------------------------- \n");
+        printf("\n |* COUNTER STRIKE MEMORY GAME *| \n");
+        printf("\n -------------------------------- \n");
+        printf("\n -------------------------------------");
+        printf("\n |1| --> Iniciar nueva partida <-- |1|");
+        printf("\n -------------------------------------");
+        printf("\n |2| -->       Salir           <-- |2| \n");
+        printf(" ------------------------------------- ");
+        printf("\n");
+        do{
+            fflush(stdin);
+            printf("\n ======================================");
+            printf("\n + Seleccione opcion. Ingrese 1 o 2: ");
+            scanf("%i",&op);
+            if(op < 1 || op > 2){
+                printf("\n");
+                printf("\n ////////////////////////////////////////////////////////////////////////// \n");
+                printf("\n |x| Error. Debe ingresar 1 o 2 para poder avanzar. Vuelva a intentarlo |x| \n");
+                printf("\n ////////////////////////////////////////////////////////////////////////// \n");
+                printf("\n");
+            }
+        } while(op < 1 || op > 2);
+        switch(op){
+            case 1:
+            break;
+        }
+    } while(op != 2);
 
     return 0;
 }
