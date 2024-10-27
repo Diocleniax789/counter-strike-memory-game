ca64275797e18a88d52955ed6ac566e84237afa8
Diocleniax789 <julioramdom123@gmail.com>
2024-10-27 02:50:38 -0300

    se han hecho los prototipos para la funcion de seleccion de bandos

--- counter-strike-memory-game/cs-memory-game.c
+++ counter-strike-memory-game/cs-memory-game.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 
 void iniciar_nuevo_juego();
+void partida_como_terroristas();
+void partida_como_anti_terroristas();
 
 int main(){
     int op;
@@ -38,4 +40,42 @@ int main(){
     } while(op != 2);
 
     return 0;
+}
+
+void iniciar_nuevo_juego(){
+    int op;
+        system("cls");
+        printf("\n ||||||||||||||||||||||||||||||||||||||||");
+        printf("\n");
+        printf("\n + SELECCIONE BANDO + \n");
+        printf("\n 1 - Terroristas.");
+        printf("\n 2 - Anti-terroristas.");
+        printf("\n 3 - Volver al menu principal.");
+        printf("\n");
+        printf("\n ||||||||||||||||||||||||||||||||||||||||");
+        printf("\n");
+    do{
+        fflush(stdin);
+        printf("\n =========================================================================");
+        printf("\n + Ingrese bando seleccionado. 1 y 2 <para los bandos> o 3 para salir: ");
+        scanf("%i",&op);
+        if(op < 1 || op > 3){
+                printf("\n");
+                printf("\n ////////////////////////////////////////////////////////////////////////// \n");
+                printf("\n |x| Error. Debe ingresar 1 o 2 o 3 para poder avanzar. Vuelva a intentarlo |x| \n");
+                printf("\n ////////////////////////////////////////////////////////////////////////// \n");
+                printf("\n");
+        }
+    } while(op < 1 || op > 3);
+    switch(op){
+        case 1:
+            // partida_como_terroristas();
+        break;
+
+        case 2:
+            // partida_como_anti_terroristas();
+        break;
+
+    }
+
 }
