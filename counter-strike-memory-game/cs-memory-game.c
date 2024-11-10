91538061abbdacda3bd45e54001793a746555ee5
Diocleniax789 <julioramdom123@gmail.com>
2024-11-10 18:28:01 -0300

    se ha solucionado el problema de que al ingresar un numero, este al estar repetido de igual forma se guardbaba

--- counter-strike-memory-game/cs-memory-game.c
+++ counter-strike-memory-game/cs-memory-game.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define FILAS 10
-#define COLUMNAS 10
-#define ELEMENTOS 5
+#define FILAS 2
+#define COLUMNAS 2
+#define ELEMENTOS 2
 
 void iniciar_nuevo_juego();
 void partida_como_terroristas();
 void partida_como_anti_terroristas();
-int numeros_ingresados(int*,int*);
+int numeros_ingresados(int*,int*,int*);
 
 int main(){
     int op;
@@ -86,9 +86,13 @@ void iniciar_nuevo_juego(){
 }
 
 void partida_como_terroristas(){
-    int contador_casillas_ocupadas = 0,j,k,grilla[FILAS][COLUMNAS],ya_ingresado,x,y;
+    int contador_casillas_ocupadas = 0,j,k,grilla[FILAS][COLUMNAS],ya_ingresado,x,y,total_casillas;
     static int numero;
     static int lista_numeros[ELEMENTOS];
+    static int pos_arreglo = 0;
+
+    total_casillas = FILAS * COLUMNAS;
+    printf("%i",total_casillas);
 
     for(j = 0; j < FILAS; j++){
         for(k = 0; k < COLUMNAS; k++){
@@ -101,36 +105,38 @@ void partida_como_terroristas(){
         system("cls");
         fflush(stdin);
         printf("\n - NO SE PERMITIRA EL INGRESO DE OTRO NUMERO YA INGRESADO ANTERIORMENTE - \n");
-        printf("\n Par nro %i",contador_casillas_ocupadas + 1);
+          printf("%i",total_casillas);
         do{
+
+            printf("\n COORDENADA NUMERO %i \n",contador_casillas_ocupadas + 1);
             printf("\n + Ingrese un numero para colocarlos en la casilla: ");
             scanf("%i",&numero);
-            ya_ingresado = numeros_ingresados(lista_numeros,&numero);
+            ya_ingresado = numeros_ingresados(lista_numeros,&numero,&pos_arreglo);
             if(ya_ingresado == 1){
                 printf("\n x ESE NUMERO YA SE HA INGRESADO ANTERIORMENTE. INTENTE CON OTRO x \n");
             }
         } while(ya_ingresado == 1);
 
-        printf("\n COORDENADA NUMERO 1 \n");
+        lista_numeros[pos_arreglo] = numero;
 
         do{
             do{
                 fflush(stdin);
                 printf("\n + Ingrese numero de la posicion X: ");
                 scanf("%i",&x);
-                if(x < 0 || x > 9){
+                if(x < 0 || x > 1){
                     printf("\n x ERROR. INGRESE UN VALOR DE ENTRE '0' y '9' x \n");
                 }
-            } while(x < 0 || x > 9);
+            } while(x < 0 || x > 1);
 
             do{
                 fflush(stdin);
                 printf("\n + Ingrese numero de la posicion Y: ");
                 scanf("%i",&y);
-                if(y < 0 || y > 9){
+                if(y < 0 || y > 1){
                     printf("\n x ERROR. INGRESE UN VALOR DE ENTRE '0' Y '9' x \n");
                 }
-            } while(y < 0 || y > 9);
+            } while(y < 0 || y > 1);
 
             if(grilla[x][y] != 0){
                 printf("\n x ERROR. ESA CASILLA YA ESTA OCUPADA x \n");
@@ -139,11 +145,11 @@ void partida_como_terroristas(){
         } while(grilla[x][y] != 0);
 
         grilla[x][y] = numero;
+        contador_casillas_ocupadas++;
 
-
-        printf("\n COORDENADA NUMERO 2 \n");
 
         do{
+                        printf("\n COORDENADA NUMERO %i \n",contador_casillas_ocupadas + 1);
             do{
                 fflush(stdin);
                 printf("\n + Ingrese numero de la posicion X: ");
@@ -157,10 +163,10 @@ void partida_como_terroristas(){
                 fflush(stdin);
                 printf("\n + Ingrese numero de la posicion Y: ");
                 scanf("%i",&y);
-                if(y < 0 || y > 9){
-                    printf("\n x ERROR. INGRESE UN VALOR DE ENTRE '0' Y '4' x \n");
+                if(y < 0 || y > 1){
+                    printf("\n x ERROR. INGRESE UN VALOR DE ENTRE '0' Y '9' x \n");
                 }
-            } while(y < 0 || y > 9);
+            } while(y < 0 || y > 1);
 
             if(grilla[x][y] != 0){
                 printf("\n x ERROR. ESA CASILLA YA ESTA OCUPADA x \n");
@@ -169,10 +175,11 @@ void partida_como_terroristas(){
         } while(grilla[x][y] != 0);
 
         grilla[x][y] = numero;
+        contador_casillas_ocupadas++;
 
         printf("\n **** NUMERO INGRESADO EN AMBAS COORDENADAS DE FORMA EXITOSA **** \n");
 
-        contador_casillas_ocupadas++;
+
 
         printf("\n");
         system("pause");
@@ -180,47 +187,41 @@ void partida_como_terroristas(){
 
 
 
-    } while(contador_casillas_ocupadas <= 20);
+    } while(contador_casillas_ocupadas < total_casillas);
 
 
 
-    /*for(j = 0; j < 4; j++){
-        for(k = 0; k < 4; k++){
-            printf("%i \t",grilla[j][k]);
+   /* for(j = 0; j < FILAS; j++){
+        for(k = 0; k < COLUMNAS; k++){
+            printf("%i | \t",grilla[j][k]);
         }
         printf("\n");
-    }*/
-
-
-
-}
-
-int numeros_ingresados(int *lista_numeros,int *numero){
-int pos = 0,flag = 0,pos_lista;
-
-if(lista_numeros[0] == 0){
-   lista_numeros[0] = *numero;
-   return 0;
-} else{
-
-    do{
-        if(*numero != lista_numeros[pos]){
-            flag = 1;
-            pos_lista = pos;
-            break;
-
-        } else{
-            pos++;
-            break;
-        }
-    } while(pos < 13 && flag == 0);
-
-    if(flag == 1){
-        lista_numeros[pos_lista] = *numero;
-        return 0;
-    } else {
-        return 1;
     }
+
+    system("pause");*/
+
 }
 
+int numeros_ingresados(int *lista_numeros,int *numero, int *pos_arreglo){
+int pos = 0,flag = 0;
+
+    if(lista_numeros[0] == 0){
+        lista_numeros[0] = *numero;
+        return 0;
+    } else{
+        do{
+            if(*numero == lista_numeros[pos]){
+                flag = 1;
+                break;
+            } else{
+                pos++;
+            }
+        } while(pos < ELEMENTOS && flag == 0);
+        if(flag == 1){
+            return 1;
+        } else{
+            (*pos_arreglo)++;
+            return 0;
+        }
+    }
 }
